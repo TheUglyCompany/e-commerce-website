7d741826ffbe2261d0b6000aa6dd87a73f2b7205
sindwarf <sindorfsolutions@gmail.com>
2022-12-12 16:16:55 -0800

    added an async test for reference and start Ratings test

--- client/src/components/__tests__/App.test.js
+++ client/src/components/__tests__/App.test.js
@@ -7,38 +7,7 @@ import App from '../App';
 
 jest.mock('axios');
 
-// // first we will use setupServer to mock an API request
-// const server = setupServer(
-//   // capture GET /reviews/ requests
-//   rest.get('/products/', (req, res, ctx) =>
-//     // respond with a mocked JSON body
-//     (res(ctx.json({
-//       id: 40344,
-//       campus: 'hr-rfp',
-//       name: 'Camo Onesie',
-//       slogan: 'Blend in to your crowd',
-//       description: 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
-//       category: 'Jackets',
-//       default_price: '140.00',
-//       created_at: '2021-08-13T14:38:44.509Z',
-//       updated_at: '2021-08-13T14:38:44.509Z',
-//     })))),
-// );
-
-// // establish API mocking before all tests
-// beforeAll(() => server.listen());
-// // reset any request handlers that are declared as a part of our tests
-// // (i.e. for testing one-time error scenarios)
-// afterEach(() => server.resetHandlers());
-// // clean up once the tests are done
-// afterAll(() => server.close());
-
-// we want to follow the format of: Arrange, Act, Assert
-// Arrange: render elements needed for test to DOM
-// Act: simulate user events
-// Assert: run assertions
-
-test('should render App component', () => {
+test.skip('should render App component', async () => {
   const testProduct = {
     data: [
       {
@@ -55,27 +24,12 @@ test('should render App component', () => {
     ],
   };
   const productResponse = Promise.resolve(testProduct);
-  act(() => {
-    axios.get.mockResolvedValue(productResponse);
-  });
-  // renders a react element into the DOM
-  // jest.spyOn(axios, 'get').mockResolvedValueOnce({
-  //   data: [
-  //     {
-  //       id: 40344,
-  //       campus: 'hr-rfp',
-  //       name: 'Camo Onesie',
-  //       slogan: 'Blend in to your crowd',
-  //       description: 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
-  //       category: 'Jackets',
-  //       default_price: '140.00',
-  //       created_at: '2021-08-13T14:38:44.509Z',
-  //       updated_at: '2021-08-13T14:38:44.509Z',
-  //     },
-  //   ],
-  // });
+
+  axios.get.mockResolvedValue(productResponse);
+
   // arrange
-  render(<App />);
+  await act(async () => render(<App />));
+
   // act
   const appElement = screen.getByTestId('app');
   // assert
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  // collectCoverageFrom: ['<roodDir>/src/**/*.{js,jsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   resetMocks: false,
+  // coveragePathIgnorePatterns: ['components/*/*.style.*'],
 };
