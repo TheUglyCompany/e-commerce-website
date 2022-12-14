45add629f3cee0441e61c82222b33fc9a0ba8e5e
Jonathan Sindorf <45885748+sindwarf@users.noreply.github.com>
2022-12-13 16:02:13 -0800

    Delete Modal.test.js

--- client/src/components/__tests__/Modal.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import Modal from '../qAndA/Modal';
-
-test.skip('Renders Modal', () => {
-  render(<Modal />);
-  const linkElement = screen.getByText(/For authentication/);
-  expect(linkElement).toBeInTheDocument();
-});
